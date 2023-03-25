b19f346279e1efcf6be550d8901e1ae0c04e3e07
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-03-25 20:28:03 +0530

    added commnets in ServerController

--- src/main/java/com/kaiburrassessment/kaiburrassessment/controller/ServerController.java
+++ src/main/java/com/kaiburrassessment/kaiburrassessment/controller/ServerController.java
@@ -18,40 +18,57 @@ public class ServerController {
 	@Autowired
 	private ServerService serverService;
 
+
+	// to quick check if the server is up and running or not
 	@GetMapping("/check")
 	public String checkingServer() {
 		return "Server is running...";
 	}
+
+
+	// This endpoint returns a list of all servers in the system.
 	@GetMapping
 	public ResponseEntity<List<Server>> getAllServers() {
 		return serverService.getAllServers();
 	}
 
+
+	// This endpoint returns a single server by its ID.
 	@GetMapping("/{serverId}")
 	public ResponseEntity<Server> getServerById(@PathVariable String serverId) {
 		return serverService.getServerById((serverId));
 	}
 
+
+	// This endpoint returns a list of servers based on their isRunning field
 	@GetMapping("/status/{isRunning}")
 	public ResponseEntity<List<Server>> getServersByRunningStatus(@PathVariable String isRunning) {
 		return serverService.getServerByIsRunning(Boolean.parseBoolean((isRunning)));
 	}
 
+
+	// This endpoint returns a single server by its name.
 	@GetMapping("/name/{name}")
 	public ResponseEntity<Server> getServersByName(@PathVariable String name) {
 		return serverService.getServerByName(name);
 	}
 
+
+	// This endpoint adds a new server to the system.
 	@PostMapping
 	public ResponseEntity<Server> addServer(@RequestBody Server server) {
 		return serverService.addServer(server);
 	}
 
+
+	// This endpoint updates an existing server.
 	@PutMapping
 	public ResponseEntity<Server> updateServer(@RequestBody Server server) {
 		return serverService.updateServer(server);
 	}
 
+
+	// This endpoint deletes a server by its ID.
 	@DeleteMapping("/{serverId}")
 	public ResponseEntity<String> deleteServer(@PathVariable String serverId) {
 		return serverService.deleteServerById(serverId);
