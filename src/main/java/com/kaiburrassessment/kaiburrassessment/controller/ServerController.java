755fc586e2955f82a52c5a1affe5deb7dd0e91c8
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-03-25 11:17:06 +0530

    all endpoints added and running good

--- src/main/java/com/kaiburrassessment/kaiburrassessment/controller/ServerController.java
+++ src/main/java/com/kaiburrassessment/kaiburrassessment/controller/ServerController.java
@@ -3,11 +3,7 @@ package com.kaiburrassessment.kaiburrassessment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.kaiburrassessment.kaiburrassessment.entities.Server;
 import com.kaiburrassessment.kaiburrassessment.services.ServerService;
@@ -15,11 +11,44 @@ import com.kaiburrassessment.kaiburrassessment.services.ServerService;
 
 
 @RestController
+@RequestMapping("/api/servers")
 public class ServerController {
 	
 	@Autowired
 	private ServerService serverService;
-	
 
-	
+	@GetMapping("/check")
+	public String checkingServer() {
+		return "Server is running...";
+	}
+	@GetMapping
+	public List<Server> getAllServers() {
+		return serverService.getAllServers();
+	}
+
+	@GetMapping("/{serverId}")
+	public Server getServerById(@PathVariable String serverId) {
+		return serverService.getServerById((serverId));
+	}
+
+	@GetMapping("/running")
+	public List<Server> getServersByRunningStatus(@RequestParam("isRunning") boolean isRunning) {
+		return serverService.getServerByIsRunning(isRunning);
+	}
+
+	@PostMapping
+	public Server addServer(@RequestBody Server server) {
+		return serverService.addServer(server);
+	}
+
+	@PutMapping
+	public Server updateServer(@RequestBody Server server) {
+		return serverService.updateServer(server);
+	}
+
+	@DeleteMapping("/{serverId}")
+	public String deleteServer(@PathVariable String serverId) {
+		return serverService.deleteServerById(serverId);
+	}
+
 }
