bfdca712a72b594cc40a9c99f22aa1ffff699e2d
Dev Bilaspure <78442088+Dev-Bilaspure@users.noreply.github.com>
2023-03-30 00:24:04 +0530

    Update PathConstants.java

--- src/main/java/com/kaiburrassessment/kaiburrassessment/constants/PathConstants.java
+++ src/main/java/com/kaiburrassessment/kaiburrassessment/constants/PathConstants.java
@@ -9,7 +9,7 @@ public class PathConstants {
     public static final String GET_ALL_SERVERS = "/api/servers";
     public static final String GET_SERVER_BY_ID = "/api/servers/{serverId}";
     public static final String GET_SERVER_BY_NAME = "/api/servers/name/{name}";
-    public static final String ADD_SERVER = "/api/servers/";
-    public static final String UPDATE_SERVER = "/api/servers/";
+    public static final String ADD_SERVER = "/api/servers";
+    public static final String UPDATE_SERVER = "/api/servers";
     public static final String DELETE_SERVER = "/api/servers/{serverId}";
 }
